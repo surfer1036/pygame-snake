19db58f8f2ee2a681824e21e6302b6b77bea5041
surfer1036 <quinlankimblebrown@gmail.com>
2017-12-04 16:21:32 -0500

    Update snake.py

--- snake.py
+++ snake.py
@@ -93,7 +93,7 @@ def get_game_over_text(score):
     This text should contain 'Game Over' as well as the score.
     score - integer representing the current score of the game.
     """
-    return 'Game Over.'
+    return 'Game Over. '
 
 def get_snake_speed(snake):
     """Return the number of cells the snake should travel in one second.
