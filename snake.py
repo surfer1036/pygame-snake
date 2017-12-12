fea72c0c624640d224c4401fff6f6b7724494c3c
surfer1036 <quinlankimblebrown@gmail.com>
2017-12-12 15:13:41 -0500

    Update snake.py

--- snake.py
+++ snake.py
@@ -46,16 +46,16 @@ def get_direction(previous_direction, event_key):
     """
     if event_key == pygame.K_LEFT:
         if previous_direction != pygame.K_RIGHT:
-        return DIRECTION_LEFT
+            return DIRECTION_LEFT
     elif event_key == pygame.K_UP:
         if previous_direction != pygame.K_DOWN:
-        return DIRECTION_UP
+            return DIRECTION_UP
     elif event_key == pygame.K_RIGHT:
         if previous_direction != pygame.K_LEFT:
-        return DIRECTION_RIGHT
+            return DIRECTION_RIGHT
     elif event_key == pygame.K_DOWN:
         if previous_direction != pygame.K_UP:
-        return DIRECTION_DOWN
+            return DIRECTION_DOWN
     return previous_direction
 
 def create_food_position():
